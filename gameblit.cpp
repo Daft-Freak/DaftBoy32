525226f5e4d5c7ab2c43caa5d0aeaaf33f74abe2
Charlie Birks <charlie@daftgames.net>
2020-03-16 16:27:37 +0000

    Partially implement sprites

--- gameblit.cpp
+++ gameblit.cpp
@@ -11,6 +11,15 @@
 DMGCPU cpu;
 
 // tmp display
+
+enum SpriteFlags
+{
+    Sprite_Palette    = (1 << 4),
+    Sprite_XFlip      = (1 << 5),
+    Sprite_YFlip      = (1 << 6),
+    Sprite_BGPriority = (1 << 7),
+};
+
 const int scanlineCycles = 456;
 int remainingScanlineCycles = scanlineCycles;
 uint8_t gbScreen[160*144];
@@ -46,13 +55,17 @@ void onCyclesExeceuted(int cycles, uint8_t *ioRegs)
     
     if(y < 144)
     {
+        const uint8_t colMap[]{0xFF, 0xCC, 0x77, 0};
+
         auto tileDataAddr = (lcdc & LCDC_TileData8000) ? 0x8000 : 0x8800;
         auto bgMapAddr = (lcdc & LCDC_BGTileMap9C00) ? 0x9C00 : 0x9800;
+        auto spriteDataAddr = 0x8000;
+
         const int tileDataSize = 16;
         const int screenSizeTiles = 32; // 32x32 tiles
         const int bgPal = ioRegs[IO_BGP];
 
-        const uint8_t colMap[]{0xFF, 0xCC, 0x77, 0};
+        const int numSprites = 40;
 
         int tileY = y / 8;
 
@@ -89,6 +102,43 @@ void onCyclesExeceuted(int cycles, uint8_t *ioRegs)
         if(lcdc & LCDC_OBJDisp)
         {
             // sprites
+
+            for(int i = 0; i < numSprites; i++)
+            {
+                const int spriteX = cpu.readMem(0xFE00/*OAM*/ + i * 4 + 1) - 8;
+                const int spriteY = cpu.readMem(0xFE00/*OAM*/ + i * 4) - 16;
+                const int tileId = cpu.readMem(0xFE00/*OAM*/ + i * 4 + 2);
+                const int attrs = cpu.readMem(0xFE00/*OAM*/ + i * 4 + 3);
+
+                const int spritePal = (attrs & Sprite_Palette) ? ioRegs[IO_OBP1] : ioRegs[IO_OBP0];
+
+                // TODO: 8x16
+                // TODO: priority
+
+                // not on this line
+                if(y < spriteY || y >= spriteY + 8)
+                    continue;
+
+
+                int ty = y - spriteY;
+
+                //TODO: flip
+
+                auto tileAddr = spriteDataAddr + tileId * tileDataSize;
+
+                // get the two tile data bytes for this line
+                uint8_t d1 = cpu.readMem(tileAddr + ty * 2);
+                uint8_t d2 = cpu.readMem(tileAddr + ty * 2 + 1);
+
+                for(int x = 0; x < 8; x++)
+                {
+                    int xShift = 7 - (x % 8); // TODO: flip
+                    int palIndex = ((d1 >> xShift) & 1) | (((d2 >> xShift) & 1) << 1);
+
+                    int col = (spritePal >> (2 * palIndex)) & 0x3;
+                    gbScreen[(x + spriteX) + y * 160] = colMap[col];
+                }
+            }
         }
     }
 }
