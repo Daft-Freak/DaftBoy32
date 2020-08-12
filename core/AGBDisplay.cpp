b8e0c6eab59f479002f482ef2b6fa582b760b020
Charlie Birks <charlie@daftgames.net>
2020-08-12 12:09:35 +0100

    Basic bitmap mode handling

--- core/AGBDisplay.cpp
+++ core/AGBDisplay.cpp
@@ -7,6 +7,54 @@
 #include "AGBMemory.h"
 #include "AGBRegs.h"
 
+static void drawBG2(int y, uint16_t *scanLine, uint16_t *palRam, uint8_t *vram, uint16_t dispControl, uint16_t control)
+{
+    switch(dispControl & DISPCNT_Mode)
+    {
+        // 0-2...
+        case 3: // 16-bit fullscreen bitmap
+        {
+            // TODO: rot/scale
+            auto inPtr = reinterpret_cast<uint16_t *>(vram + y * 240 * 2);
+            auto outPtr = scanLine;
+            for(int x = 0; x < 240; x++)
+                *outPtr++ = *inPtr++;
+            break;
+        }
+        case 4: // paletted fullscreen bitmap
+        {
+            auto inPtr = vram + y * 240;
+            auto outPtr = scanLine;
+            for(int x = 0; x < 240; x++)
+                *outPtr++ = palRam[*inPtr++];
+            break;
+        }
+        case 5: // 16-bit 160*128 bitmap
+        {
+            // TODO: rot/scale
+            auto outPtr = scanLine;
+            if(y < 128)
+            {
+                auto inPtr = reinterpret_cast<uint16_t *>(vram + y * 160 * 2);
+
+                int x;
+                for(x = 0; x < 160; x++)
+                    *outPtr++ = *inPtr++;
+
+                // fill rest with pal 0? (works for tonc demo)
+                for(;x < 240; x++)
+                    *outPtr++ = palRam[0];
+            }
+            else
+                for(int x = 0; x < 240; x++)
+                    *outPtr++ = palRam[0];
+            break;
+        }
+        default:
+            memset(scanLine, 0, 240 * 2);
+    }
+}
+
 AGBDisplay::AGBDisplay(AGBCPU &cpu) : cpu(cpu), mem(cpu.getMem())
 {
 }
@@ -75,5 +123,17 @@ bool AGBDisplay::writeReg(uint32_t addr, uint8_t data)
 
 void AGBDisplay::drawScanLine(int y)
 {
+    auto dispControl = mem.readIOReg(IO_DISPCNT);
+    auto bg2Control = mem.readIOReg(IO_BG2CNT);
+
+    auto palRAM = reinterpret_cast<uint16_t *>(mem.getPalRAM());
+    auto vram = mem.getVRAM();
+
     auto scanLine = screenData + y * screenWidth;
+
+    for(int priority = 3; priority >= 0; priority--)
+    {
+        if((dispControl & DISPCNT_BG2On) && (bg2Control & BGCNT_Priority) == priority)
+            drawBG2(y, scanLine, palRAM, vram, dispControl, bg2Control);
+    }
 }
--- core/AGBRegs.h
+++ core/AGBRegs.h
@@ -3,8 +3,13 @@
 enum AGBIOReg
 {
     // LCD
+    IO_DISPCNT = 0x0,
     IO_DISPSTAT = 0x4,
     IO_VCOUNT = 0x6,
+    IO_BG0CNT = 0x8,
+    IO_BG1CNT = 0xA,
+    IO_BG2CNT = 0xC,
+    IO_BG3CNT = 0xE,
 
     // input
     IO_KEYINPUT = 0x130,
@@ -13,6 +18,24 @@ enum AGBIOReg
     IO_IE = 0x200,
     IO_IF = 0x202,
     IO_IME = 0x208,
+};
+
+enum DISPCNTBits
+{
+    DISPCNT_Mode       = 0x7,
+
+    DISPCNT_Frame       = (1 << 4),
+    DISPCNT_HBlankFree  = (1 << 5),
+    DISPCNT_OBJChar1D   = (1 << 6),
+    DISPCNT_ForceBlank  = (1 << 7),
+    DISPCNT_BG0On       = (1 << 8),
+    DISPCNT_BG1On       = (1 << 9),
+    DISPCNT_BG2On       = (1 << 10),
+    DISPCNT_BG3On       = (1 << 11),
+    DISPCNT_OBJOn       = (1 << 12),
+    DISPCNT_Window0On   = (1 << 13),
+    DISPCNT_Window1On   = (1 << 14),
+    DISPCNT_OBJWindowOn = (1 << 15),
 };
 
 enum DISPSTATBits
@@ -25,4 +48,10 @@ enum DISPSTATBits
     DISPSTAT_VCountInt  = 1 << 5
 
     //8-15 is VCount value to compare
+};
+
+enum BGCNTBits
+{
+    BGCNT_Priority = 0x3,
+    //...
 };
