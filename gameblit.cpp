efd9244da2ebba4460a0cac85e53fb853d51f697
Charlie Birks <charlie@daft.games>
2021-08-23 15:56:25 +0100

    BGRGB

--- gameblit.cpp
+++ gameblit.cpp
@@ -166,7 +166,7 @@ void packedToRGB(const uint8_t *packed_data, uint8_t *data)
                         for (int c = 0; c <= count; c++)
                         {
 #ifdef PICO_BUILD
-                            uint16_t packed = (palette[col].b >> 3) | ((palette[col].g >> 2) << 5) | ((palette[col].r >> 3) << 11);
+                            uint16_t packed = (palette[col].r >> 3) | ((palette[col].g >> 2) << 5) | ((palette[col].b >> 3) << 11);
                             *pdest++ = packed & 0xFF;
                             *pdest++ = packed >> 8;
 #else
