ae7fe28d716ef5b2caa1d1b312a65ab6877e3dde
Charlie Birks <charlie@daft.games>
2022-08-04 19:15:02 +0100

    Move setup out of recompileExInstruction
    Allows deleting all the duplicated helpers

--- core/DMGRecompiler.h
+++ core/DMGRecompiler.h
@@ -36,7 +36,7 @@ private:
     bool compile(uint8_t *&codePtr, uint16_t pc, BlockInfo &blockInfo);
 
     bool recompileInstruction(uint16_t &pc, OpInfo &instr, X86Builder &builder);
-    void recompileExInstruction(OpInfo &instr, X86Builder &builder, int &cyclesThisInstr, int &delayedCyclesExecuted);
+    void recompileExInstruction(OpInfo &instr, X86Builder &builder);
 
     void compileEntry();
 
