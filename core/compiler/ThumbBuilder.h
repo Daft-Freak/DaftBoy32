04e2e5e9a8a0f90aa1551b031faf596cb0302e0f
Charlie Birks <charlie@daft.games>
2022-08-06 17:32:23 +0100

    Jump into and out of compiled thumb code
    Still not enough to do anything

--- core/compiler/ThumbBuilder.h
+++ core/compiler/ThumbBuilder.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cassert>
 #include <cstdint>
 
 enum class Reg
@@ -26,17 +27,16 @@ enum class Reg
     PC = R15,
 };
 
-// first 8
-enum class LowReg
+// wrapper that asserts reg is in range
+class LowReg final
 {
-    R0 = 0,
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
-    R7
+public:
+    constexpr LowReg(Reg r): val(r)
+    {
+        assert(static_cast<int>(r) < 8);
+    }
+
+    Reg val;
 };
 
 class ThumbBuilder final
@@ -44,7 +44,17 @@ class ThumbBuilder final
 public:
     ThumbBuilder(uint16_t *ptr, uint16_t *endPtr) : ptr(ptr), endPtr(endPtr){}
 
+    void bl(int32_t off);
+
     void bx(Reg r);
+
+    void mov(LowReg r, uint8_t imm);
+
+    void orr(LowReg d, LowReg m);
+
+    void pop(uint8_t regList, bool pc);
+
+    void push(uint8_t regList, bool lr);
 
     uint16_t *getPtr() const {return ptr;}
 
