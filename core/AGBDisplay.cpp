acf3ffd40a8bc3247c0d96fdd235e1fb81446a29
Charlie Birks <charlie@daft.games>
2023-05-25 12:49:48 +0100

    Fix a few missing includes

--- core/AGBDisplay.cpp
+++ core/AGBDisplay.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cstring>
+#include <tuple>
 
 #include "AGBDisplay.h"
 
--- minsdl/Main.cpp
+++ minsdl/Main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <thread>
 #include <unordered_map>
 
