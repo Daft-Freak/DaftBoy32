6706784cd4a35282b22d6e88685611d4e2e02260
Charlie Birks <charlie@daft.games>
2023-05-26 19:39:27 +0100

    ...further hackery so it actually runs things

--- core/compiler/DMGRecompilerGeneric.cpp
+++ core/compiler/DMGRecompilerGeneric.cpp
@@ -4,6 +4,8 @@
 
 DMGRecompilerGeneric::DMGRecompilerGeneric(DMGCPU &cpu) : DMGRecompiler(cpu), fallback(cpu)
 {
+    fallback.codeBuf = codeBuf;
+    fallback.curCodePtr = codeBuf;
 }
 
 bool DMGRecompilerGeneric::compile(uint8_t *&codePtr, uint16_t pc, BlockInfo &blockInfo) 
@@ -17,8 +19,32 @@ void DMGRecompilerGeneric::compileEntry()
     fallback.compileEntry();
 
     auto len = fallback.curCodePtr - fallback.codeBuf;
-    memcpy(codeBuf, fallback.codeBuf, len);
     curCodePtr += len;
+
+    // patch tmpSavedPtr/exitForCall
+    auto ptr = fallback.saveAndExitPtr + 4;
+    uint64_t addr = reinterpret_cast<uintptr_t>(&tmpSavedPtr);
+
+    *ptr++ = addr;
+    *ptr++ = addr >> 8;
+    *ptr++ = addr >> 16;
+    *ptr++ = addr >> 24;
+    *ptr++ = addr >> 32;
+    *ptr++ = addr >> 40;
+    *ptr++ = addr >> 48;
+    *ptr++ = addr >> 56;
+
+    ptr = fallback.exitForCallPtr + 2;
+    addr = reinterpret_cast<uintptr_t>(&exitCallFlag);
+
+    *ptr++ = addr;
+    *ptr++ = addr >> 8;
+    *ptr++ = addr >> 16;
+    *ptr++ = addr >> 24;
+    *ptr++ = addr >> 32;
+    *ptr++ = addr >> 40;
+    *ptr++ = addr >> 48;
+    *ptr++ = addr >> 56;
 
     entryFunc = reinterpret_cast<CompiledFunc>(codeBuf);
 }
