66aed726c183dcca84a15bf844a15d8fe5978b34
Charlie Birks <charlie@daft.games>
2023-06-07 16:51:31 +0100

    Add shifts with CL to X86Builder

--- core/compiler/X86Builder.h
+++ core/compiler/X86Builder.h
@@ -191,6 +191,7 @@ public:
 
     void ror(Reg8 r, uint8_t count);
 
+    void sarCL(Reg8 dst);
     void sar(Reg8 r, uint8_t count);
 
     void sbb(Reg8 dst, Reg8 src);
@@ -198,9 +199,11 @@ public:
 
     void setcc(Condition cc, Reg8 dst);
 
+    void shrCL(Reg8 dst);
     void shr(Reg32 r, uint8_t count);
     void shr(Reg8 r, uint8_t count);
 
+    void shlCL(Reg8 dst);
     void shl(Reg32 r, uint8_t count);
     void shl(Reg8 r, uint8_t count);
 
