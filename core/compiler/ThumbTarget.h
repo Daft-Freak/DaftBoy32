1ef91b9824efabe14f45c207025e7f2a0a60b35f
Charlie Birks <charlie@daft.games>
2023-06-17 12:03:32 +0100

    Stub ThumbTarget
    DMGRecompilerGeneric builds now at least

---
+++ core/compiler/ThumbTarget.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <map>
+#include <optional>
+
+#include "RecompilerGeneric.h"
+
+enum class Reg;
+
+class ThumbTarget final
+{
+public:
+    ThumbTarget(){}
+
+    void init(SourceInfo sourceInfo, void *cpuPtr);
+
+    const SourceInfo &getSourceInfo() {return sourceInfo;}
+
+    bool compile(uint8_t *&codePtr, uint8_t *codeBufEnd, uint16_t pc, GenBlockInfo &blockInfo);
+
+    void compileEntry(uint8_t *&codeBuf, unsigned int codeBufSize);
+
+private:
+    SourceFlagInfo &getFlagInfo(SourceFlagType flag);
+    uint8_t flagWriteMask(SourceFlagType flag);
+    bool writesFlag(uint16_t opFlags, SourceFlagType flag);
+
+    SourceInfo sourceInfo;
+    void *cpuPtr;
+
+    std::map<uint8_t, Reg> regAlloc;
+
+    uint8_t flagsReg = 0;
+    uint8_t flagMap[4]; // map from SourceFlagType to flags bit
+
+    uint16_t *exitPtr = nullptr, *saveAndExitPtr = nullptr, *exitForCallPtr = nullptr;
+};
