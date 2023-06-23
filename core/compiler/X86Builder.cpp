4a32d7554abde4904bc446525d9cf64d659c00d6
Charlie Birks <charlie@daft.games>
2023-06-23 19:12:51 +0100

    Add 32-bit SAR to X86Builder

--- core/compiler/X86Builder.cpp
+++ core/compiler/X86Builder.cpp
@@ -713,6 +713,16 @@ void X86Builder::ror(Reg8 r, uint8_t count)
     write(count);
 }
 
+// reg by CL
+void X86Builder::sarCL(Reg32 r)
+{
+    auto reg = static_cast<int>(r);
+
+    encodeREX(false, 0, 0, reg);
+    write(0xD3); // opcode, w = 1
+    encodeModRM(reg, 7);
+}
+
 // reg by CL, 8bit
 void X86Builder::sarCL(Reg8 r)
 {
@@ -721,6 +731,19 @@ void X86Builder::sarCL(Reg8 r)
     encodeREX(false, 0, 0, reg);
     write(0xD2); // opcode
     encodeModRM(reg, 7);
+}
+
+// reg
+void X86Builder::sar(Reg32 r, uint8_t count)
+{
+    auto reg = static_cast<int>(r);
+
+    // TODO: 0xD0 for 1
+
+    encodeREX(false, 0, 0, reg);
+    write(0xC1); // opcode, w = 1
+    encodeModRM(reg, 7);
+    write(count);
 }
 
 // reg, 8 bit
